e1d54eee38df0c150e6a81c93e9c56471bc1422f
olensis <olensis7gmail@.com>
2022-09-07 13:00:23 +0300

    домашка готова

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        {
+            var task = 1;
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+            }
+            var task2 = 2;
+            for (int l = 10; l > 0; l--) {
+                System.out.println( l );
+
+                var task3 = 3;
+                for ( int k = k= 0; k <17; k++ )
+                if (k % 2 ==0)
+                {
+                    System.out.println( k );}
+
+                    var task4 = 4;
+                    for  (int m = 10; m >= -10; m--)
+                    {
+                        System.out.println( m);}
+
+                    var task5 = 5;
+                    for (int o = 1904; o <= 2096; o = o + 4)
+                        System.out.println( o+ " является високосным");
+
+                var task6 = 6;
+                for (int n =7; n<=98; n= n+7 )
+                    System.out.println(n);
+
+                var task7 = 7;
+                for (int v = 1; v<=512; v= v * 2)
+                    System.out.println(v);
+
+                var task8 =8;
+                int salary = 29000;
+                int total = 0;
+                for (int i = 1; i <= 12; i ++) {
+
+                    total = total + salary;
+                    System.out.println("за " + i + " месяц " + total);
+                }
+
+                    var task9 =9;
+                    int salary1 = 29000;
+                    int total1 = 0;
+                    for (int h = 1; h <= 12; h ++) {
+                        total1 = total1 + total / 100;
+                        total1 = total1 + +salary1;
+                        System.out.println(" за " + h + "сумма накоплений" + total1);
+                    }
+                {
+
+
+                    }
+
+                }
+
+
+
+                        {
+                }
+
+                    {
+                }
+
+            }
+
+        }
     }
-}
